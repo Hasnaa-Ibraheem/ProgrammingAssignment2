150d39f450fb3c7565c2d7e6adb23a7b531f1db8
Hasnaa-Ibraheem <hasnaa.ibraheem@gmail.com>
2016-09-18 07:09:35 +0200

    Adding my functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Prepare data matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+if(nrow(x) != ncol(x)){
+    warning("Cacheable matrix must be square matrix!")
+    return
+  }
+  
+  inversedMtrx <- NULL
+  set <- function(c) {
+    if(nrow(c) != ncol(c)){
+      warning("Cacheable matrix must be square matrix!")
+      return
+    }
+    x <<- c
+  }
+  get <- function() x
+  
+  setInversedMatrix <- function(invMtx) inversedMtrx <<- invMtx
+  getInversedMatrix <- function() inversedMtrx
+  
+  mtxObj <- list(set = set, get = get, 
+       setInversedMatrix = setInversedMatrix,
+       getInversedMatrix = getInversedMatrix)
+  invisible(mtxObj)
 }
 
 
-## Write a short comment describing this function
+## Solve and cache the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       solvedMtx <- x$getInversedMatrix()
+  if(!is.null(solvedMtx)) {
+    message("Getting cached data...")
+    return(solvedMtx)
+  }
+  data <- x$get()
+  solvedMtx <- solve(data, ...)
+  x$setInversedMatrix(solvedMtx)
+  message("First time to set solved matrix data")
+  solvedMtx
 }
